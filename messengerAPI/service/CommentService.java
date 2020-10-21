d0c9f042619be59b6b63264b760d21c1d365b071
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-10-21 13:59:18 +0530

    Added a webApplication Exceptions

--- messengerAPI/service/CommentService.java
+++ messengerAPI/service/CommentService.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.sanket.messengerAPI.database.DatabaseClass;
 import org.sanket.messengerAPI.model.Comment;
+import org.sanket.messengerAPI.model.ErrorMessage;
 import org.sanket.messengerAPI.model.Message;
 
 public class CommentService {
@@ -18,8 +24,20 @@ public class CommentService {
 	}
 
 	public Comment getComment(long messageId, long commentId) {
+		ErrorMessage errorMessage = new ErrorMessage("NOt FOUND", 404, "http://google.com"); // handle the exceptions 4
+																								// lines of code
+		Response response = Response.status(Status.NOT_FOUND).entity(errorMessage).build(); //
+
+		Message message = messages.get(messageId);
+		if (message == null) {
+			throw new WebApplicationException(response);
+		}
 		Map<Long, Comment> comments = messages.get(messageId).getComments();
-		return comments.get(commentId);
+		Comment comment = comments.get(commentId);
+		if (comment == null) {
+			throw new NotFoundException(response); // shortcut to handle the not found exceptions.
+		}
+		return comment;
 	}
 
 	public Comment addComment(long messageId, Comment comment) {
