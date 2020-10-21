01b0f9ec3ac3bcf7d45e4d26a847d49067cfde16
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-10-21 13:27:55 +0530

    Added a separate package to handle Exceptions

---
+++ messengerAPI/exception/DataNotFoundException.java
@@ -0,0 +1,14 @@
+package org.sanket.messengerAPI.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9117821438221630547L;
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+
+}
